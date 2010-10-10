0fa64b09710b8aa16f0144270e7120ae5dc82757
Shawn-in-Tokyo <family@192.168.1.21>
2010-10-11 08:50:10 +0900

    needed class to autoOn bt and changed message

---
+++ MINDdroid/src/com/lego/minddroid/EnableBT.java
@@ -0,0 +1,77 @@
+package com.lego.minddroid;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+
+public class EnableBT extends Activity {
+
+	boolean prcessStarted = false;
+	StatusReciever statusReciever;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setResult(Activity.RESULT_CANCELED);
+
+		statusReciever = new StatusReciever();
+		registerReceiver(statusReciever, new IntentFilter("android.bluetooth.adapter.action.STATE_CHANGED"));
+		prcessStarted = turnOnBt();
+		if (!prcessStarted) {
+			sendFailureStatus();
+		}
+	}
+
+	private boolean turnOnBt() {
+
+		return BluetoothAdapter.getDefaultAdapter().enable();
+	}
+
+	public void sendFailureStatus() {
+
+		Log.d("EnableBT sendFailureStatus", "RESULT_CANCELED");
+		this.setResult(Activity.RESULT_CANCELED);
+		finish();
+	}
+
+	public void sendSuccessStatus() {
+		Log.d("EnableBT sendSuccessStatus", "RESULT_OK");
+		this.setResult(Activity.RESULT_OK);
+		finish();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		unregisterReceiver(statusReciever);
+	}
+
+	public class StatusReciever extends BroadcastReceiver {
+
+		/**
+		 * 
+		 */
+
+		public final static String STATE_CHANGED = "android.bluetooth.adapter.action.STATE_CHANGED";
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.d("EnableBT statusReciever", "onReceive");
+			if (intent.getAction().equals(STATE_CHANGED)) {
+				Log.d("EnableBT statusReciever", "ACTION_STATE_CHANGED");
+				sendSuccessStatus();
+			} else {
+				Log.d("EnableBT statusReciever", "fail: " + intent.getAction());
+				sendFailureStatus();
+
+			}
+
+		}
+	}
+
+}
