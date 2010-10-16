1a23ecfa5d050c7031af53d2b925a356af74f67c
Guenther Hoelzl <ghoelzl@gmail.com>
2010-10-16 18:59:35 +0000

    added header in Options.java

--- MINDdroid/src/com/lego/minddroid/Options.java
+++ MINDdroid/src/com/lego/minddroid/Options.java
@@ -1,3 +1,22 @@
+/**
+ *   Copyright 2010 Guenther Hoelzl, Shawn Brown
+ *
+ *   This file is part of MINDdroid.
+ *
+ *   MINDdroid is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   MINDdroid is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with MINDdroid.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
 package com.lego.minddroid;
 
 import android.app.Activity;
