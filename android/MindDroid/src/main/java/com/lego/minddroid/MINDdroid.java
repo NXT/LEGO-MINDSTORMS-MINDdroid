9cd3f24ff6dae4aa2a82fa1cabb8ac752bde1c2a
Hannes Achleitner <hannes.software@gmx.at>
2019-12-28 13:32:51 +0100

    extract byte function

--- android/MindDroid/src/main/java/com/lego/minddroid/MINDdroid.java
+++ android/MindDroid/src/main/java/com/lego/minddroid/MINDdroid.java
@@ -77,11 +77,28 @@ public class MINDdroid extends Activity implements BTConnectable, TextToSpeech.O
     private List<String> programList;
     private static final int MAX_PROGRAMS = 20;
     private String programToStart;
-    private Toast reusableToast;
 
     // experimental TTS support
     private TextToSpeech mTts;
     private final int TTS_CHECK_CODE = 9991;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRobotType = this.getIntent().getIntExtra(SplashMenu.MINDDROID_ROBOT_TYPE, R.id.robot_type_shooterbot);
+        setUpByType();
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        StartSound mySound = new StartSound(this);
+        mySound.start();
+        // setup our view, give it focus and display.
+        mView = new GameView(getApplicationContext(), this);
+        mView.setFocusable(true);
+        setContentView(mView);
+
+        // experimental TTS support for the lejosMINDdroid project
+        mTts = new TextToSpeech(this, this);
+    }
 
     /**
      * Asks if bluetooth was switched on during the runtime of the app. For saving
@@ -108,25 +125,6 @@ public class MINDdroid extends Activity implements BTConnectable, TextToSpeech.O
     @Override
     public boolean isPairing() {
         return pairing;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mRobotType = this.getIntent().getIntExtra(SplashMenu.MINDDROID_ROBOT_TYPE, R.id.robot_type_shooterbot);
-        setUpByType();
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        StartSound mySound = new StartSound(this);
-        mySound.start();
-        // setup our view, give it focus and display.
-        mView = new GameView(getApplicationContext(), this);
-        mView.setFocusable(true);
-        setContentView(mView);
-        reusableToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
-
-        // experimental TTS support for the lejosMINDdroid project
-        mTts = new TextToSpeech(this, this);
     }
 
     /**
@@ -524,27 +522,12 @@ public class MINDdroid extends Activity implements BTConnectable, TextToSpeech.O
         return false;
     }
 
-    /**
-     * Displays a message as a toast
-     *  @param textToShow the message
-     *
-     */
-    private void showToast(String textToShow, int length) {
-        reusableToast.setText(textToShow);
-        reusableToast.setDuration(length);
-        reusableToast.show();
+    private void showToast(String textToShow) {
+        Toast.makeText(this, textToShow, Toast.LENGTH_SHORT).show();
     }
 
-    /**
-     * Displays a message as a toast
-     *
-     * @param resID  the ressource ID to display
-     * @param length the length of the toast to display
-     */
     private void showToast(int resID, int length) {
-        reusableToast.setText(resID);
-        reusableToast.setDuration(length);
-        reusableToast.show();
+        Toast.makeText(this, resID, Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -565,26 +548,23 @@ public class MINDdroid extends Activity implements BTConnectable, TextToSpeech.O
                     sendBTCmessage(BTCommunicator.NO_DELAY, BTCommunicator.GET_FIRMWARE_VERSION, 0, 0);
                     break;
                 case BTCommunicator.MOTOR_STATE:
-
                     if (myBTCommunicator != null) {
                         byte[] motorMessage = myBTCommunicator.getReturnMessage();
-                        int position = byteToInt(motorMessage[21]) + (byteToInt(motorMessage[22]) << 8) + (byteToInt(motorMessage[23]) << 16)
-                                + (byteToInt(motorMessage[24]) << 24);
+                        int position = ByteHelper.byteToInt(motorMessage[21]) +
+                                (ByteHelper.byteToInt(motorMessage[22]) << 8) +
+                                (ByteHelper.byteToInt(motorMessage[23]) << 16) +
+                                (ByteHelper.byteToInt(motorMessage[24]) << 24);
                         showToast(getResources().getString(R.string.current_position) + position, Toast.LENGTH_SHORT);
                     }
-
                     break;
-
                 case BTCommunicator.STATE_CONNECTERROR_PAIRING:
                     connectingProgressDialog.dismiss();
                     destroyBTCommunicator();
                     break;
-
                 case BTCommunicator.STATE_CONNECTERROR:
                     connectingProgressDialog.dismiss();
                 case BTCommunicator.STATE_RECEIVEERROR:
                 case BTCommunicator.STATE_SENDERROR:
-
                     destroyBTCommunicator();
                     if (!btErrorPending) {
                         btErrorPending = true;
@@ -599,11 +579,8 @@ public class MINDdroid extends Activity implements BTConnectable, TextToSpeech.O
                                 });
                         builder.create().show();
                     }
-
                     break;
-
                 case BTCommunicator.FIRMWARE_VERSION:
-
                     if (myBTCommunicator != null) {
                         byte[] firmwareMessage = myBTCommunicator.getReturnMessage();
                         // check if we know the firmware
@@ -621,11 +598,8 @@ public class MINDdroid extends Activity implements BTConnectable, TextToSpeech.O
                         // afterwards we search for all files on the robot
                         sendBTCmessage(BTCommunicator.NO_DELAY, BTCommunicator.FIND_FILES, 0, 0);
                     }
-
                     break;
-
                 case BTCommunicator.FIND_FILES:
-
                     if (myBTCommunicator != null) {
                         byte[] fileMessage = myBTCommunicator.getReturnMessage();
                         String fileName = new String(fileMessage, 4, 20);
@@ -638,57 +612,22 @@ public class MINDdroid extends Activity implements BTConnectable, TextToSpeech.O
                         // find next entry with appropriate handle,
                         // limit number of programs (in case of error (endless loop))
                         if (programList.size() <= MAX_PROGRAMS)
-                            sendBTCmessage(BTCommunicator.NO_DELAY, BTCommunicator.FIND_FILES,
-                                    1, byteToInt(fileMessage[3]));
+                            sendBTCmessage(BTCommunicator.NO_DELAY, BTCommunicator.FIND_FILES, 1, ByteHelper.byteToInt(fileMessage[3]));
                     }
-
                     break;
-
                 case BTCommunicator.PROGRAM_NAME:
                     if (myBTCommunicator != null) {
                         byte[] returnMessage = myBTCommunicator.getReturnMessage();
                         startRXEprogram(returnMessage[2]);
                     }
-
                     break;
-
                 case BTCommunicator.SAY_TEXT:
                     if (myBTCommunicator != null) {
-                        byte[] textMessage = myBTCommunicator.getReturnMessage();
-                        // evaluate control byte
-                        byte controlByte = textMessage[2];
-                        // BIT7: Language
-                        if ((controlByte & 0x80) == 0x00)
-                            mTts.setLanguage(Locale.US);
-                        else
-                            mTts.setLanguage(Locale.getDefault());
-                        // BIT6: Pitch
-                        if ((controlByte & 0x40) == 0x00)
-                            mTts.setPitch(1.0f);
-                        else
-                            mTts.setPitch(0.75f);
-                        // BIT0-3: Speech Rate
-                        switch (controlByte & 0x0f) {
-                            case 0x01:
-                                mTts.setSpeechRate(1.5f);
-                                break;
-                            case 0x02:
-                                mTts.setSpeechRate(0.75f);
-                                break;
-
-                            default:
-                                mTts.setSpeechRate(1.0f);
-                                break;
-                        }
-
-                        String ttsText = new String(textMessage, 3, 19);
-                        ttsText = ttsText.replaceAll("\0", "");
-                        showToast(ttsText, Toast.LENGTH_SHORT);
-                        mTts.speak(ttsText, TextToSpeech.QUEUE_FLUSH, null);
+                        String resultText = ByteHelper.handleResult(mTts, myBTCommunicator.getReturnMessage());
+                        showToast(resultText);
+                        mTts.speak(resultText, TextToSpeech.QUEUE_FLUSH, null);
                     }
-
                     break;
-
                 case BTCommunicator.VIBRATE_PHONE:
                     if (myBTCommunicator != null) {
                         byte[] vibrateMessage = myBTCommunicator.getReturnMessage();
@@ -697,20 +636,10 @@ public class MINDdroid extends Activity implements BTConnectable, TextToSpeech.O
                             myVibrator.vibrate(vibrateMessage[2] * 10);
                         }
                     }
-
                     break;
             }
         }
     };
-
-    private int byteToInt(byte byteValue) {
-        int intValue = (byteValue & (byte) 0x7f);
-
-        if ((byteValue & (byte) 0x80) != 0)
-            intValue |= 0x80;
-
-        return intValue;
-    }
 
     void selectNXT() {
         Intent serverIntent = new Intent(this, DeviceListActivity.class);
